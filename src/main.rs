014bcb19f6be3d0e90e049018aa31746c20c17f7
young <young20050727@gmail.com>
2024-07-10 05:20:32 +0800

    feat: add text color from origin package manager

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::process::Stdio;
-use std::io::{BufReader, BufRead};
+use std::process::{Stdio};
 
 use fluent::FluentBundle;
 use unic_langid::langid;
@@ -41,27 +40,12 @@ fn install(manager: String, package: String) {
     }
 
     println!("yu: Installing package: {}", package);
-    // install package using package manager
     let mut cmd = syntax::gen_install_syntax(manager.clone())
         .arg(package)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .spawn()
         .expect("yu: Failed to execute command");
-
-    let stdout = cmd.stdout.take().expect("Failed to capture stdout");
-    let stderr = cmd.stderr.take().expect("Failed to capture stderr");
-
-    let stdout_reader = BufReader::new(stdout);
-    let stderr_reader = BufReader::new(stderr);
-
-    for line in stdout_reader.lines() {
-        println!("{}", line.unwrap());
-    }
-
-    for line in stderr_reader.lines() {
-        eprintln!("{}", line.unwrap());
-    }
 
     cmd.wait().expect("Command wasn't running");
 }
@@ -73,77 +57,32 @@ fn uninstall(manager: String, package: String) {
     }
 
     println!("yu: Uninstalling package: {}", package);
-    // uninstall package using package manager
     let mut cmd = syntax::gen_uninstall_syntax(manager.clone())
         .arg(package)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .spawn()
         .expect("yu: Failed to execute command");
-
-    let stdout = cmd.stdout.take().expect("Failed to capture stdout");
-    let stderr = cmd.stderr.take().expect("Failed to capture stderr");
-
-    let stdout_reader = BufReader::new(stdout);
-    let stderr_reader = BufReader::new(stderr);
-
-    for line in stdout_reader.lines() {
-        println!("{}", line.unwrap());
-    }
-
-    for line in stderr_reader.lines() {
-        eprintln!("{}", line.unwrap());
-    }
 
     cmd.wait().expect("Command wasn't running");
 }
 
 fn upgrade(manager: String) {
-    // update
     println!("yu: Updating system");
     let mut update_cmd = syntax::gen_update_syntax(manager.clone())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .spawn()
         .expect("Failed to execute update command");
 
-    let update_stdout = update_cmd.stdout.take().expect("Failed to capture update stdout");
-    let update_stderr = update_cmd.stderr.take().expect("Failed to capture update stderr");
-
-    let stdout_reader = BufReader::new(update_stdout);
-    let stderr_reader = BufReader::new(update_stderr);
-
-    for line in stdout_reader.lines() {
-        println!("{}", line.unwrap());
-    }
-
-    for line in stderr_reader.lines() {
-        eprintln!("{}", line.unwrap());
-    }
-
     update_cmd.wait().expect("Update command wasn't running");
 
-    // upgrade
     println!("yu: Upgrading system");
     let mut upgrade_cmd = syntax::gen_upgrade_syntax(manager.clone())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .spawn()
         .expect("Failed to execute upgrade command");
-
-    let upgrade_stdout = upgrade_cmd.stdout.take().expect("Failed to capture upgrade stdout");
-    let upgrade_stderr = upgrade_cmd.stderr.take().expect("Failed to capture upgrade stderr");
-
-    let stdout_reader = BufReader::new(upgrade_stdout);
-    let stderr_reader = BufReader::new(upgrade_stderr);
-
-    for line in stdout_reader.lines() {
-        println!("{}", line.unwrap());
-    }
-
-    for line in stderr_reader.lines() {
-        eprintln!("{}", line.unwrap());
-    }
 
     upgrade_cmd.wait().expect("Upgrade command wasn't running");
 
