93b3aa597636046d0c0ed07810c97f0dfeb3b4a4
Young <young20050727@gmail.com>
2024-09-22 03:13:58 +0800

    test: add test for root.rs

--- src/root.rs
+++ src/root.rs
@@ -10,3 +10,43 @@ pub fn get_sudo(manager: String) -> std::process::Command {
 
     command
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_sudo() {
+        let manager = "apt".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "dnf".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "yum".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "pacman".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "zypper".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "apk".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "portage".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "sudo");
+
+        let manager = "brew".to_string();
+        let cmd = get_sudo(manager.clone());
+        assert_eq!(cmd.get_program(), "brew");
+    }
+}
